15b8306e46131d969463d5e5cb1f1c9668c36e2c
musetronstar <icolley@fastmail.fm>
2013-12-01 23:04:18 -0700

    added error template

--- httagd/include/httagd.h
+++ httagd/include/httagd.h
@@ -28,14 +28,14 @@ class tagl_callback : public TAGL::callback {
 		void cmd_get(const tagd::abstract_tag&);
 		void cmd_put(const tagd::abstract_tag&);
 		void cmd_query(const tagd::interrogator&); 
-        void error(const TAGL::driver&);
+        void cmd_error(const TAGL::driver&);
 };
 
-class template_callback : public TAGL::callback {
+class template_callback : public TAGL::callback, public tagd::errorable {
 		TAGL::driver *_driver;
 		tagspace::tagspace *_TS;
 		evhtp_request_t *_req;
-		std::string _tpl_file;
+		std::string _opt_tpl;
 		std::string _context;
 		bool _trace_on;
 
@@ -44,17 +44,17 @@ class template_callback : public TAGL::callback {
 				tagspace::tagspace* ts,
 				TAGL::driver *drvr,
 				evhtp_request_t* req,
-				const std::string& tpl_file,
+				const std::string& opt_tpl,
 				const std::string& context,
 				bool trace = false
 			) : _TS{ts}, _driver{drvr}, _req{req},
-			_tpl_file{tpl_file}, _context{context}, _trace_on{trace}
+			_opt_tpl{opt_tpl}, _context{context}, _trace_on{trace}
 		{}
 
 		void cmd_get(const tagd::abstract_tag&);
 		void cmd_put(const tagd::abstract_tag&);
 		void cmd_query(const tagd::interrogator&); 
-        void error(const TAGL::driver&);
+        void cmd_error(const TAGL::driver&);
 };
 
 
