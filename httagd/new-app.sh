3b06633052684b555ecb0f55f8f093b6be0729fc
musetronstar <inc@hypermega.com>
2022-05-31 17:43:04 +0700

    added usage

--- httagd/new-app.sh
+++ httagd/new-app.sh
@@ -3,7 +3,13 @@
 set -e	# exit on error
 
 DST_DIR=$1
-[ -d $DST_DIR ] || mkdir -p $DST_DIR
+
+if [ -n $DST_DIR ]; then
+	echo "usage: new-app.sh <dest-dir>" 2>&1
+	exit 1
+fi
+
+[ -d "$DST_DIR" ] || mkdir -p "$DST_DIR"
 
 DST_APP_DIR=${DST_DIR}/app
 if [ -d $DST_APP_DIR ]; then
@@ -16,8 +22,7 @@ fi
 # this scripts working dir
 PRJ_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")/../" >/dev/null 2>&1 && pwd )"
 PRJ_APP_DIR=${PRJ_DIR}/httagd/app
-cp -r ${PRJ_APP_DIR}/* $DST_APP_DIR
-find $DST_APP_DIR -name '*.o' -exec rm '{}' \;
+rsync -av --progress ${PRJ_APP_DIR}/* $DST_APP_DIR --exclude '*.o'
 
 cat ${PRJ_APP_DIR}/Makefile \
 	| sed "s|PRG_DIR = .*|PRG_DIR = ${PRJ_DIR}|g" > ${DST_APP_DIR}/Makefile
